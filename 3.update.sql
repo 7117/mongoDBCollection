a4e8fb76e69485ec1e1e8bd2580f4cd6b933f17b
7117 <qinglangsalaheiyou@163.com>
2020-06-06 17:37:38 +0800

    code

---
+++ 3.update.sql
@@ -0,0 +1,14 @@
+-- 全部替换
+db.stus.update({"name":"aa"},{"age":3000})
+
+
+-- 局部更新
+db.stus.update(
+    {"_id":ObjectId("5edb4595ce5d733096ad4cdb")},
+    {
+        $set:{
+            "age":"2000",
+            "name":"name"
+        }
+    }
+)
